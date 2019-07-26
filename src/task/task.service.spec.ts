12449599ddc81ea7151b8ca8013c0ddfcd24572a
Kataev Vitaliy <vita@kataev.pro>
2019-07-26 19:07:15 +0300

    Add ci config (#1)
    * Add ci config
    * fix tests

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TaskEntity } from './task.entity';
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [
+        {
+          provide: getRepositoryToken(TaskEntity),
+          useClass: Repository,
+        },
+        TaskService],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
